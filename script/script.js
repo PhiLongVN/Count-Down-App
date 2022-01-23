4086e70affeb1988d32f7bd01e4195aec95611e5
Phi Long <91886384+PhiLongVN@users.noreply.github.com>
2022-01-23 17:18:45 +0700

    upload

---
+++ script/script.js
@@ -0,0 +1,35 @@
+/* ============================================ */
+/*                  COUNT DOWN                  */
+/* ============================================ */
+let future = new Date("02,01,2022").getTime();
+
+const secondText = document.querySelector(".second");
+const minuteText = document.querySelector(".minute");
+const hourText = document.querySelector(".hour");
+const dayText = document.querySelector(".day");
+
+function getTime() {
+  let now = new Date().getTime();
+  let distance = future - now;
+
+  //   cong thuc tinh time !IMPORTANCE
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //   render ra
+  secondText.innerText = seconds;
+  minuteText.innerText = minutes;
+  hourText.innerText = hours;
+  dayText.innerText = days;
+
+  if (distance < 0) {
+    clearInterval(count);
+  }
+}
+
+// goi ham moi 1s
+let count = setInterval(() => {
+  getTime();
+}, 1000);
